8ebe31f6689d6b73f2b99cdac4af71270f0e97d2
Adam Shields <adamenote@gmail.com>
2020-01-14 16:26:38 -0500

    Added basic models and superuser created

--- _scratch.py
+++ _scratch.py
@@ -2,19 +2,19 @@ from django.db import models
 
 class Team(models.Model):
     """
-    Supports a multiple Projects
-    Supports multiple Servers within that project
+    Supports a Project
+    Supports Servers within that project
     """
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
-
+        
 class Server(models.Model):
     """
-    Servers are associated with Teams 
-    Server can have multiple Teams that support it
-    Servers are associated with a single project
+    What Team Supports the Server
+    What Project its under
+    Server Can only be associated with 1 project
     """
     name = models.CharField(max_length=200)
 
@@ -23,9 +23,8 @@ class Server(models.Model):
 
 class Project(models.Model):
     """
-    Projects need show the Teams that Support the Project
-    Projects need to show Servers associated with the Project
-
+    What servers are under the project
+    what team supports the project
     """
     name = models.CharField(max_length=200)
 
--- src/dashboard/admin.py
+++ src/dashboard/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Team, Server, Project
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Team
+
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Server
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Project
