30cfc3e87613354b0fcd2f0c9b498cf829b60ade
Adam Shields <adamenote@gmail.com>
2020-01-15 10:55:06 -0500

    changed admin

--- src/dash2/admin.py
+++ src/dash2/admin.py
@@ -4,26 +4,41 @@ from django.contrib import admin
 from .models import Team, Server, Project, ProjectManager
 
 
-@admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Team
 
-@admin.register(Server)
+admin.site.register(Team, TeamAdmin)
+
+class TeamTabularInline(admin.TabularInline):
+    model = Team
+
 class ServerAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Server
 
-@admin.register(Project)
+admin.site.register(Server, ServerAdmin)
+
+class ServerTabularInline(admin.TabularInline):
+    model = Server
+
 class ProjectAdmin(admin.ModelAdmin):
 
     class Meta:
         model = Project
 
-@admin.register(ProjectManager)
+admin.site.register(Project, ProjectAdmin)
+
+class ProjectTabularInline(admin.TabularInline):
+    model = Project
+
 class ProjectManagerAdmin(admin.ModelAdmin):
 
     class Meta:
         model = ProjectManager
+
+admin.site.register(ProjectManager, ProjectManagerAdmin)
+
+
