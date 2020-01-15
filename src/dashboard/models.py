e6b514d6c8b600541f605cc876d6adaa8085e9e0
Adam Shields <adamenote@gmail.com>
2020-01-14 21:20:02 -0500

    Dashboard Working

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -11,8 +11,6 @@ class Team(models.Model):
     Supports multiple Servers within that project
     """
     name = models.CharField(max_length=200)
-    # server = models.ManyToManyField("Server", related_name="team_server", verbose_name="Supported Servers", blank=True)
-    # project = models.ManyToManyField("Project", related_name="team_projects", verbose_name="Supported Projects", blank=True)
 
 
     def __str__(self):
@@ -25,9 +23,6 @@ class Server(models.Model):
     Servers are associated with a single project
     """
     name = models.CharField(max_length=200)
-    # team = models.ManyToManyField("Team", related_name="team", verbose_name="Associated Teams for the Server", blank=True)
-    # # team = models.ManyToManyField("Team", related_name="team_server", verbose_name="Associated Teams for the Server", blank=True) # BACKUP Change related_name
-    # project = models.ForeignKey("Project", related_name="server_project", verbose_name="Associated Project", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -39,21 +34,21 @@ class Project(models.Model):
 
     """
     name = models.CharField(max_length=200)
-    # team = models.ManyToManyField("Team", related_name="team", verbose_name="Teams that Support the Project", blank=True)
-    # # team = models.ManyToManyField("Team", related_name="team_servers", verbose_name="Teams that Support the Project", blank=True) # BACKUP Change related_name
-    # server = models.ManyToManyField("Server", related_name="project_servers", verbose_name="Servers associated with the Project", blank=True)
 
     def __str__(self):
         return self.name
 
-
 class ProjectManager(models.Model):
-    project_name = models.ForeignKey("Project", on_delete=models.CASCADE)
-    project_team_servers = models.ManyToManyField("Team", related_name="projects_team_servers", verbose_name="Teams that Support the Project", blank=True)
-    project_servers = models.ManyToManyField("Server", related_name="servers_related_to_project", verbose_name="Associated Supported Servers", blank=True)
-	
+    
+    name = models.ForeignKey(Project, on_delete=models.CASCADE)
+    team = models.ManyToManyField(Team, related_name="team_server", verbose_name="Teams that Support the Project", blank=True)
+    server = models.ManyToManyField(Server, related_name="project_server", verbose_name="Associated Supported Servers", blank=True)
+    
+    # def __str__(self):
+    #     return self.id
+        
     def __str__(self):
-        return self.project_name
+        return str(self.name)
         
     def get_absolute_url(self):
 		#url(r'^book/(?P<id>\d+)$', BookDetail.as_view(), name='book_detail'),
